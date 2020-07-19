05c4cc2de05eb52fbe550e062e06107cbc778dec
Pawan Paudel <pawanpaudel93@gmail.com>
2020-07-19 19:25:37 +0545

    added pageload progressbar to top

--- src/main.js
+++ src/main.js
@@ -1,15 +1,27 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
 import Vuelidate from 'vuelidate'
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbvue/lib/css/mdb.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import InfiniteLoading from 'vue-infinite-loading';
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbvue/lib/css/mdb.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import InfiniteLoading from 'vue-infinite-loading'
+import VueProgressBar from 'vue-progressbar'
+
+const options = {
+  color: 'rgb(143, 255, 199)',
+  thickness: '2px',
+  transition: {
+    speed: '1.5s',
+    opacity: '0.6s',
+    termination: 400
+  },
+}
 
 Vue.use(InfiniteLoading);
 Vue.use(Vuelidate);
+Vue.use(VueProgressBar, options)
 
 Vue.config.productionTip = false;
 
